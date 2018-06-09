f52438e596182bf75afb12bdff4fde856c34f6a5
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-09 07:27:42 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,6 +7,7 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.create(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
   end
 
   def show
